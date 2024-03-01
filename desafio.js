e71b9c3a071f8ab3d82cbb6305c78905c3bbf997
xavierlima.ti <xavierlima.ti@gmail.com>
2024-03-01 12:27:38 -0300

    Desafio2

---
+++ desafio.js
@@ -0,0 +1,25 @@
+function olaMundo() {
+    console.log('Olá Mundo!');
+}
+
+olaMundo();
+
+function olaNome(nome) {
+    console.log(`Olá, ${nome}!`);
+}
+
+olaNome("Gustavo")
+
+function dobroNumero(numero) {
+    return numero * 2;
+}
+
+let dobro = dobroNumero(2);
+    console.log(dobro);
+
+function calculoMedia(x, y, z) {
+    return (x + y + z) / 3
+}
+
+let media = calculoMedia(1, 2, 3);
+console.log(media);
